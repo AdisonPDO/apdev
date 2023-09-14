a9d43984f434b2670687707f92040299e472501f
AdisonP <adison.pereira@epitech.eu>
2023-09-14 09:29:01 +0200

    first

---
+++ app/pages/Services.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Services() {
+  return (
+    <div className="bg-graymap text-center py-16"> 
+      <h1 className="text-5xl text-white font-bold mb-4">MES SERVICES</h1>
+      <h4 className="text-2xl text-gray-400 font-bold mb-8">Des prestations adaptées à vos besoins</h4>
+      <div className="bg-cover bg-center h-64" style={{ backgroundImage: 'url(/bg_map.png)' }}></div>
+    </div>
+  )
+}
