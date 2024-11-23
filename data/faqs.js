f11174019f8eb66118e5c89cb5aa115c7bf29f5c
Adison Pereira <52446207+AdisonPDO@users.noreply.github.com>
2024-11-23 12:23:22 +0100

    up

---
+++ data/faqs.js
@@ -0,0 +1,82 @@
+export const faqs = [
+  {
+    id: 1,
+    question: "01. Création de sites web",
+    answer:
+        "Je crée des sites web modernes et performants, adaptés aux besoins de votre entreprise. Que ce soit pour une vitrine ou un site e-commerce, j’optimise chaque projet pour une visibilité maximale sur le web, notamment à Perpignan.",
+  },
+  {
+    id: 2,
+    question: "02. Développement d'applications mobiles",
+    answer:
+        "Je développe des applications mobiles sur mesure, conçues pour offrir une expérience utilisateur fluide et intuitive. C’est idéal pour toucher un public connecté, que vous soyez à Perpignan ou à l’échelle nationale.",
+  },
+  {
+    id: 3,
+    question: "03. Optimisation SEO",
+    answer:
+        "J’améliore votre visibilité en ligne grâce à des stratégies SEO efficaces. En ciblant des mots-clés spécifiques à Perpignan et à votre activité, je vous aide à apparaître en tête des résultats de recherche.",
+  },
+  {
+    id: 4,
+    question: "04. Maintenance et support technique",
+    answer:
+        "Je m’assure que vos sites web et applications fonctionnent parfaitement en proposant des services de maintenance, des mises à jour régulières, et un support technique réactif.",
+  },
+];
+
+
+
+
+
+
+export const faqDataMain = [
+  {
+    id: 1,
+    question: "What do you mean by an End Product?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Ut laoreet sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur velit pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus.",
+  },
+  {
+    id: 2,
+    question: "Where do I find my Purchase or License code?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Ut laoreet sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur velit pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus.",
+  },
+  {
+    id: 3,
+    question: "Do I need to buy a licence for each site?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Ut laoreet sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur velit pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus.",
+  },
+  {
+    id: 4,
+    question: "Is my license transferable?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula porttitor rutrum. Ut laoreet sodales rutrum. Ut eget tristique nibh. Cras mattis est eget lacus dignissim, non consectetur velit pellentesque. Ut sit amet placerat eros, id gravida arcu. Sed felis purus, tempor ac vestibulum nec, consectetur at purus.",
+  },
+];
+export const faqs3 = [
+  {
+    question: "Our Story",
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            condimentum, mauris a bibendum cursus, risus quam venenatis ipsum,
+            in facilisis tellus lacus ac augue. Fusce placerat condimentum
+            luctus. Pellentesque venenatis lacus eu ex sollicitudin maximus.
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. Vestibulum rhoncus eros vitae enim lobortis
+            cursus. Suspendisse potenti. Ut sed tortor ut dolor consequat congue
+            a lacinia ante. Phasellus vel urna eu lacus luctus luctus vitae quis`,
+  },
+  {
+    question: "Our Mission",
+    answer: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            condimentum, mauris a bibendum cursus, risus quam venenatis ipsum,
+            in facilisis tellus lacus ac augue. Fusce placerat condimentum
+            luctus. Pellentesque venenatis lacus eu ex sollicitudin maximus.
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+            per inceptos himenaeos. Vestibulum rhoncus eros vitae enim lobortis
+            cursus. Suspendisse potenti. Ut sed tortor ut dolor consequat congue
+            a lacinia ante. Phasellus vel urna eu lacus luctus luctus vitae quis`,
+  },
+];
